ca61660fab6421a097e583ea82363487d0389d55
Leon <silenceace@gmail.com>
2020-04-20 13:51:35 +0800

    fixed

--- custom_scripts/after_pull/run.sh
+++ custom_scripts/after_pull/run.sh
@@ -10,17 +10,17 @@ notify_all "AfterPull"
 # install deps
 echo "installing deps..."
 if [ -n "$INSTALL_DEPS_COMMAND" ]; then
-    eval "$INSTALL_DEPS_COMMAND || (echo \"Installing deps failed. Aborting!\"; $(notify_error); exit 1)"
+    eval "$INSTALL_DEPS_COMMAND || echo "Installing deps failed. Aborting!"; $\(notify_error\); exit 1"
 else
-    npm install || (echo "Installing deps failed. Aborting!"; $(notify_error); exit 1)
+    npm install || echo "Installing deps failed. Aborting!"; $(notify_error); exit 1
 fi
 
 # build code
 echo "building code..."
 if [ -n "$BUILD_COMMAND" ]; then
-    eval "$BUILD_COMMAND || (echo \"Build failed. Aborting!\"; $(notify_error); exit 1)"
+    eval "$BUILD_COMMAND || echo "Build failed. Aborting;"; $\(notify_error\) ; exit 1"
 else
-    npm run build || (echo "Build failed. Aborting!"; $(notify_error); exit 1)
+    npm run build || echo "Build failed. Aborting;" ; $(notify_error) ; exit 1
 fi
 
 # move target
@@ -33,8 +33,6 @@ fi
 echo "moving to target dir done"
 
 
-set +e
-
 # calc package elasped time
 elasped_package_time "end"
 # record current git commit id
@@ -43,8 +41,10 @@ echo $(parse_git_hash) > /tmp/CURRENT_GIT_COMMIT_ID
 # after package command
 if [ -n "$AFTER_PACKAGE_COMMANDS" ]; then
     echo "after package command do..." 
-    eval "$AFTER_PACKAGE_COMMANDS"
+    eval "$AFTER_PACKAGE_COMMANDS || echo "After Package Command failed. Aborting!"; $\(notify_error\); exit 1"
 fi
+
+set +e
 
 echo "after package shell do..." 
 source /usr/bin/run_scripts_after_package.sh
